72920973ca838ba4e020f41a7d85a359bc3ce40d
nut_tharin <37585893+nuttharin@users.noreply.github.com>
2021-06-06 11:15:04 +0700

    test โรงงาน

--- env.js
+++ env.js
@@ -1,8 +1,13 @@
-var ipAddress = {
+var IP_ADDRESS = {
 
     //URL_API_MID  : "192.168.1.132:8080",
-    URL_API_MID   : "192.168.250.13:8080",
+    URL_API_MID   : "34.87.0.193:8080",
     IP_MACHINE :  "192.168.1.156:5000" ,
-    side  : "#369"
+    side  : "#369",
   
   };
+
+const DATA_FIX = {
+  MACHINE_CODE : "GASSHOP14230" ,
+  MACHINE_ID : "1" 
+}
